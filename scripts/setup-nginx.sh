f613eb08543126e16bcaae8f5ed543e88c11325f
Caio Domingos <43622756+Caio-Domingos@users.noreply.github.com>
2025-02-10 11:23:02 -0300

    update nginx + pm2 install

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Definir valores padrão para o domínio e a porta
-VAR_1="example.com"
-VAR_2="90"
-VAR_2="3000"
-
-# Solicitar ao usuário para substituir os valores padrão, se desejado
-read -p "Entre com o domínio [$VAR_1]: " input_var_1
-read -p "Entre com a porta do frontend [$VAR_2]: " input_var_2
-read -p "Entre com a porta do backend [$VAR_3]: " input_var_3
-
-# Substituir os valores padrão com os valores fornecidos pelo usuário, se existirem
-VAR_1="${input_var_1:-$VAR_1}"
-VAR_2="${input_var_2:-$VAR_2}"
-VAR_3="${input_var_3:-$VAR_3}"
-
-
-
 # Atualizar sistema
 cd ~
 sudo yum update -y
@@ -29,84 +12,31 @@ sudo service nginx start
 sudo chkconfig nginx on
 sudo service nginx stop
 
-echo "
+# Criar configuração do Nginx com valores fixos
+sudo tee /etc/nginx/conf.d/myapp.conf > /dev/null <<EOL
 server {
     listen 80;
-    server_name $VAR_1;
-    return 301 https://\$host\$request_uri;
-}
-
-server {
-    listen 443 ssl;
-    server_name $VAR_1;
-
-    ssl_certificate /root/.acme.sh/$VAR_1/fullchain.cer;
-    ssl_certificate_key /root/.acme.sh/$VAR_1/$VAR_1.key;
+    server_name example.com;
 
     location / {
-        proxy_pass http://localhost:$VAR_2;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    }
-    location /api {
-        proxy_pass http://localhost:$VAR_3;
+        proxy_pass http://localhost:8080;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 }
-"
+EOL
 
-# Executar os comandos como root
+# Reiniciar o Nginx para aplicar as configurações
+sudo service nginx restart
 
-# Instalar pacotes de agendamento
-# yum install cronie socat
+#!/bin/bash
 
-# Instalar o acme.sh
-# curl https://get.acme.sh | sh
+# Atualizar o sistema
+cd ~
+sudo yum update -y
 
-# Criar link simbólico para o acme.sh
-# ln -s /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
+# Instalar o PM2 globalmente com npm
+npm install -g pm2
 
-# Configurar o acme com o meu email
-# acme.sh --register-account -m caio.domingos@thinkless.com.br
-
-# Emitir o certificado SSL
-# acme.sh --issue --standalone -d $VAR_1
-
-# Editar o arquivo de configuração do Nginx
-# sudo vi /etc/nginx/conf.d/myapp.conf
-
-# Adicionar as configurações do servidor
-
-# server {
-#     listen 80;
-#     server_name $VAR_1;
-#     return 301 https://\$host\$request_uri;
-# }
-
-# server {
-#     listen 443 ssl;
-#     server_name $VAR_1;
-
-#     ssl_certificate /root/.acme.sh/$VAR_1_ecc/fullchain.cer;
-#     ssl_certificate_key /root/.acme.sh/$VAR_1_ecc/$VAR_1.key;
-
-#     location / {
-#         proxy_pass http://localhost:$VAR_2;
-#         proxy_set_header Host \$host;
-#         proxy_set_header X-Real-IP \$remote_addr;
-#         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-#     }
-#     location /api {
-#         proxy_pass http://localhost:$VAR_3;
-#         proxy_set_header Host \$host;
-#         proxy_set_header X-Real-IP \$remote_addr;
-#         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-#     }
-# }
-
-
-# Reiniciar o Nginx
-# e
+pm2 startup systemd
